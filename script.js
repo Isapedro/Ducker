d1c041d57bc1feb4da39ceaf8c4b3b587dd593d2
Isapedro <isa.pedro92@hotmail.com>
2024-01-25 14:25:50 +0100

    grid drawed

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const playAgainBtn = document.querySelector(".play-again");
 
 const gridMatrix = [
   ["", "", "", "", "", "", "", "", ""],
-  ["river", "wood", "wood", "river", "wood", "river", "river", "river", "wood"],
-  ["wood", "river", "wood", "wood", "river", "river", "wood", "river", "wood"],
+  ["river", "wood", "river", "river", "wood", "river", "river", "river", "wood"],
+  ["river", "river", "wood", "wood", "river", "river", "wood", "river", "wood"],
   ["", "", "", "", "", "", "", "", ""],
-  ["road", "bus", "road", "road", "road", "car", "road", "road", "road"],
-  ["road", "bus", "road", "car", "road", "road", "road", "road", "bus"],
+  ["road", "bus", "road", "road", "road", "car", "road", "road", "car"],
+  ["road", "bus", "road", "car", "road", "road", "bus", "road", "bus"],
   ["road", "road", "car", "road", "road", "bus", "road", "road", "car"],
   ["", "", "", "", "", "", "", "", ""],
   ["", "", "", "", "", "", "", "", ""]
@@ -27,8 +27,27 @@ let contentBeforeDuck = "";
 let time = 15;
 
 function drawGrid(){
-  grid.innerHTML="";
+  grid.innerHTML= "";
 
   //for each row in the matrix, we need to process what is going to be drawn/displyzed on the screen
-  gridMatrix.forEach
+  gridMatrix.forEach(function (gridRow, gridRowIndex){
+    gridRow.forEach(function (cellContent, cellContentIndex){
+      //given the current grid row, create a cell for the grid in the game based ont the cellcontent
+      const cellDiv = document.createElement("div");
+      cellDiv.classList.add("cell");
+//[1,2]
+if ( riverRows.includes(gridRowIndex)){
+cellDiv.classList.add("river");
+//[4, 5, 6]
+} else if (roadRows.includes(gridRowIndex)){
+cellDiv.classList.add("road");
 }
+if(cellContent) {
+  cellDiv.classList.add(cellContent)
+}
+
+      grid.appendChild(cellDiv);
+    });
+  });
+}
+drawGrid()
